365ee86bedeae68bf92f90d552cbf5f4d343dcea
David Wells <daverwells@gmail.com>
2013-08-30 22:55:38 -0400

    remove unused commands; add old macro code for ORDER()

--- cmaster.el
+++ cmaster.el
@@ -1,11 +1,16 @@
 ;; set up linux style and then switch to indentation of 8.
 (setq c-default-style "linux" c-basic-offset 8)
 
-;; c-lineup-arglist-close-under-paren
-;; (c-set-offset 'arglist-cont '(c-lineup-arglist-operators 0))
-;; (c-set-offset 'arglist-cont-nonempty '(c-lineup-arglist-operators
-;;                                         c-lineup-arglist))
-;; (c-set-offset 'arglist-close '(c-lineup-arglist-close-under-paren))
-
 ; fix behavior of > and < in normal state.
 (set (make-local-variable 'evil-shift-width) 8)
+
+(defun add-order-macro (start end)
+  "Convert some pair 1, 2 to ORDER(1, 2, entriesPerRow, entriesPerCol)"
+  (defun decrement-string (integer-string)
+    (number-to-string (- (parse-integer integer-string) 1)))
+  (interactive "r")
+  (save-restriction
+    (narrow-to-region start end)
+    (goto-char start)
+    (replace-regexp "\\([0-9]+\\),\\([0-9]+\\)"
+                    "ORDER(\\1, \\2, entriesPerRow, entriesPerCol)")))
