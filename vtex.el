fffe20e1fa378eefa318588eb94c521c7186eba5
David Wells <daverwells@gmail.com>
2013-09-01 10:54:48 -0400

    update to use prog mode, and the new indentation code

--- vtex.el
+++ vtex.el
@@ -1,12 +1,18 @@
+;; this file is NOT part of GNU emacs.
 (defvar vtex-mode-hook nil)
 
-(defvar vtex-mode-map
-  (let ((vtex-mode-map (make-keymap)))
-    (define-key vtex-mode-map "\C-j" 'newline-and-indent)
-    vtex-mode-map)
-  "Keymap for VTEX major mode")
+(defconst vtex-version 0.1
+  "alpha copy of vtex.")
+
+(defun vtex-comment-dwim (arg)
+  "Comment blocks via newcomment."
+  (interactive "*P")
+  (require 'newcomment)
+  (let ((comment-start "%") (comment-end ""))
+    (comment-dwim arg)))
 
 (defconst vtex-font-lock-keywords-1
+  ; just so that I can check what colors do what.
   (list
    ; match \\
    '("\\(\\\\\\\\\\)" 1 font-lock-negation-char-face)
@@ -22,9 +28,9 @@
    '("\\(jjjjj\\)" 1 font-lock-preprocessor-face)
    '("\\(kkkkk\\)" 1 font-lock-string-face)
    '("\\(lllll\\)" 1 font-lock-doc-face)
-   '("\\(mmmmm\\)" 1 font-lock-negation-char-face)
-   ; match comments (start with %)
-   '("[^\\]\\(%.*$\\)" 1 font-lock-comment-face))
+   '("\\(mmmmm\\)" 1 font-lock-constant-face)
+   '("\\(nnnnn\\)" 1 font-lock-negation-char-face)
+   '("\\(ooooo\\)" 1 font-lock-reference-face))
    "Minimal highlighting expressions for VTEX mode.")
 
 (defconst vtex-font-lock-sectioning
@@ -34,87 +40,134 @@
         '("section{\\(.*\\)}" 1 font-lock-builtin-face))
   "Additional Keywords to highlight in VTEX mode.")
 
-; These are some possible built-in values for VTEX attributes
-; "ROLE" "ORGANISATIONAL_UNIT" "STRING" "REFERENCE" "AND"
-; "XOR" "WORKFLOW" "SYNCHR" "NO" "APPLICATIONS" "BOOLEAN"
-; "INTEGER" "HUMAN" "UNDER_REVISION" "OR"
-(defconst vtex-font-lock-keywords-2
-  (list
-   '("\\<\\(A\\(ND\\|PPLICATIONS\\)\\|BOOLEAN\\|HUMAN\\|INTEGER\\|NO\\|OR\\(GANISATIONAL_UNIT\\)?\\|R\\(EFERENCE\\|OLE\\)\\|S\\(TRING\\|YNCHR\\)\\|UNDER_REVISION\\|WORKFLOW\\|XOR\\)\\>" . font-lock-constant-face))
-  "extra highlighting in VTEX mode.")
+(defconst vtex-font-lock-math-delimiters
+  (list '("\\\\[]()[]" . font-lock-string-face))
+  "Math delimiters.")
 
 (defconst vtex-font-lock-catch-backslash
   (list '("\\(\\\\\\w+\\)" . font-lock-builtin-face))
   "catch additional backslashed terms.")
 
-; these have to be done in order. We want to have:
-; 1. the \\ command (that is, carriage returns)
-; 2. known commands
-; 3. unknown commands
 (defvar vtex-font-lock-keywords
-  (append vtex-font-lock-keywords-1
+  (append vtex-font-lock-math-delimiters
           vtex-font-lock-sectioning
-          vtex-font-lock-keywords-2
+          vtex-font-lock-keywords-1
           vtex-font-lock-catch-backslash)
   "Default highlighting expressions for VTEX mode.")
 
-;; (defun vtex-indent-line ()
-;;   "Indent current line as VTEX code."
-;;   (interactive)
-;;   (beginning-of-line)
-;;   (if (bobp)
-;; 	  (indent-line-to 0)		   ; First line is always non-indented
-;; 	(let ((not-indented t) cur-indent)
-;; 	  (if (looking-at "^[ \t]*END_") ; If the line we are looking at is the end of a block, then decrease the indentation
-;; 		  (progn
-;; 			(save-excursion
-;; 			  (forward-line -1)
-;; 			  (setq cur-indent (- (current-indentation) default-tab-width)))
-;; 			(if (< cur-indent 0) ; We can't indent past the left margin
-;; 				(setq cur-indent 0)))
-;; 		(save-excursion
-;; 		  (while not-indented ; Iterate backwards until we find an indentation hint
-;; 			(forward-line -1)
-;; 			(if (looking-at "^[ \t]*END_") ; This hint indicates that we need to indent at the level of the END_ token
-;; 				(progn
-;; 				  (setq cur-indent (current-indentation))
-;; 				  (setq not-indented nil))
-;; 			  (if (looking-at "^[ \t]*\\(PARTICIPANT\\|MODEL\\|APPLICATION\\|WORKFLOW\\|ACTIVITY\\|DATA\\|TOOL_LIST\\|TRANSITION\\)") ; This hint indicates that we need to indent an extra level
-;; 				  (progn
-;; 					(setq cur-indent (+ (current-indentation) default-tab-width)) ; Do the actual indenting
-;; 					(setq not-indented nil))
-;; 				(if (bobp)
-;; 					(setq not-indented nil)))))))
-;; 	  (if cur-indent
-;; 		  (indent-line-to cur-indent)
-;; 		(indent-line-to 0))))) ; If we didn't see an indentation hint, then allow no indentation
+(defvar vtex-syntax-table
+  (let ((vtex-mode-syntax-table (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" vtex-mode-syntax-table)
+    (modify-syntax-entry ?% "< b" vtex-mode-syntax-table)
+    (modify-syntax-entry ?\n "> b" vtex-mode-syntax-table)
+    vtex-mode-syntax-table)
+  "Syntax table for VTEX mode.")
 
-;; (defvar vtex-mode-syntax-table
-;;   (let ((vtex-mode-syntax-table (make-syntax-table)))
+(defvar vtex-mode-map
+  (let ((vtm (make-keymap)))
+    (define-key vtm "\C-j" 'newline-and-indent)
+    (define-key vtm [remap comment-dwim] 'vtex-comment-dwim)
+    (define-key vtm [remap comment-region] 'vtex-comment-dwim)
+    vtm)
+  "Keymap for VTEX major mode")
 
-;;     ; This is added so entity names with underscores can be more easily parsed
-;; 	(modify-syntax-entry ?_ "w" vtex-mode-syntax-table)
-
-;; 	; Comment styles are same as C++
-;; 	(modify-syntax-entry ?/ ". 124b" vtex-mode-syntax-table)
-;; 	(modify-syntax-entry ?* ". 23" vtex-mode-syntax-table)
-;; 	(modify-syntax-entry ?\n "> b" vtex-mode-syntax-table)
-;; 	vtex-mode-syntax-table)
-;;   "Syntax table for vtex-mode")
-
-(defun vtex-mode ()
+(defun vtex-matching-begin-indent ()
+  "Assuming that the cursor is currently at an 'end' statement, find the
+   indentation level of the matching begin statement."
   (interactive)
-  (kill-all-local-variables)
-  (use-local-map vtex-mode-map)
-  ;; (set-syntax-table vtex-mode-syntax-table)
-  ;; Set up font-lock
-  (set (make-local-variable 'font-lock-defaults) '(vtex-font-lock-keywords))
-  ;; Register our indentation function
-  ;; (set (make-local-variable 'indent-line-function) 'vtex-indent-line)
-  (setq major-mode 'vtex-mode)
-  (setq mode-name "VTEX")
-  (run-hooks 'vtex-mode-hook))
+  (save-excursion
+  (defvar stack-count 1)
+  (setq stack-count 1)
+    (while (and (not (bobp)) (not (eq stack-count 0)))
+      (forward-line -1)
+      (beginning-of-line)
+      (if (search-forward "\\begin" (line-end-position) t)
+          (setq stack-count (- stack-count 1))
+      (if (search-forward "\\end" (line-end-position) t)
+          (setq stack-count (+ 1 stack-count)))))
+    (current-indentation)))
+
+(defun vtex-fix-end-block ()
+  (save-excursion
+    (beginning-of-line)
+    (if (looking-at "^ *\\\\end")
+      (progn (message "found end block")
+             (indent-line-to (vtex-matching-begin-indent))))))
+
+(defun vtex-indent-line ()
+  (interactive)
+  (defvar vtex-next-indent 0)
+  (setq vtex-max-look-count 2)
+  (setq vtex-look-count 1)
+  (save-excursion
+    (progn
+      (forward-line -1)
+      (beginning-of-line)
+      (cond
+       ((bobp) (indent-line-to 0))
+       ((looking-at "^ *\\\\section") (progn
+                                        (indent-line-to 0)
+                                        (setq vtex-next-indent 4)))
+       ((looking-at "^ *\\\\subsection") (progn
+                                           (indent-line-to 4)
+                                           (setq vtex-next-indent 8)))
+       ((looking-at "^ *\\\\subsubsection") (progn
+                                              (indent-line-to 8)
+                                              (setq vtex-next-indent 12)))
+       ((looking-at "^.*\\\\begin") (progn
+                                      (setq vtex-next-indent
+                                            (+ 4 (current-indentation)))))
+       ((looking-at "^.*\\\\end") (progn
+                                    (indent-line-to
+                                     (vtex-matching-begin-indent))
+                                    (setq vtex-next-indent
+                                          (current-indentation))))
+       ((looking-at "^ *$") (progn (while (and (eq (current-indentation) 0)
+                                               (< vtex-look-count
+                                                  vtex-max-look-count)
+                                               (not (bobp)))
+                                     (forward-line -1)
+                                     (setq vtex-look-count
+                                           (1+ vtex-look-count)))
+                                   (setq vtex-next-indent
+                                         (current-indentation))))
+       (t (setq vtex-next-indent (current-indentation))))))
+  (indent-line-to vtex-next-indent)
+  ; end blocks are badly behaved. Check it again.
+  (vtex-fix-end-block))
+
+(defun vtex-indent-region (start end)
+  "Based on my preferences for how indentation works (i.e. fix
+   section locations while indenting), override the usual
+   indent-region command."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+    (narrow-to-region start end)
+      (goto-char 0)
+      (while (not (eobp))
+        (vtex-indent-line)
+        (forward-line 1)
+        (end-of-line)))))
+
+(define-derived-mode vtex-mode prog-mode
+  "Major mode for editing LaTeX files."
+  :syntax-table vtex-syntax-table
+  (progn
+    (setq font-lock-defaults vtex-font-lock-keywords)
+    (setq comment-start "%")
+    (setq comment-end "")
+    (use-local-map vtex-mode-map)
+    (set (make-local-variable 'font-lock-defaults) '(vtex-font-lock-keywords))
+    (set (make-local-variable 'indent-line-function) 'vtex-indent-line)
+    (set (make-local-variable 'indent-region) 'vtex-indent-region)
+    (setq major-mode 'vtex-mode)
+    (setq mode-name "VTEX")
+    (add-hook 'before-save-hook (font-lock-fontify-buffer))
+    (add-hook 'before-save-hook (lambda ()
+                                  (setq font-lock-mode-major-mode nil)
+                                  (font-lock-fontify-buffer)))
+    (run-hooks 'vtex-mode-hook)))
 
 (provide 'vtex-mode)
-
 ;;; vtex-mode.el ends here
