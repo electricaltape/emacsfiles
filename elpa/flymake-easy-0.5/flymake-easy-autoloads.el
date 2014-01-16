6cce818506c326f9cf6fb9d20778d67dabe94bd3
David Wells <daverwells@gmail.com>
2014-01-16 06:02:31 -0500

    deleted old package files

--- elpa/flymake-easy-0.5/flymake-easy-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; flymake-easy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("flymake-easy-pkg.el" "flymake-easy.el")
-;;;;;;  (20833 33930 507132 78000))
-
-;;;***
-
-(provide 'flymake-easy-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-easy-autoloads.el ends here
