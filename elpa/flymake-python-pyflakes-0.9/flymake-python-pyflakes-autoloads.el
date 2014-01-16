f144ef633010adedd31ef3b51e9796894520ff40
David Wells <daverwells@gmail.com>
2014-01-16 06:04:17 -0500

    oops, forgot a package

---
+++ elpa/flymake-python-pyflakes-0.9/flymake-python-pyflakes-autoloads.el
@@ -0,0 +1,29 @@
+;;; flymake-python-pyflakes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flymake-python-pyflakes-load) "flymake-python-pyflakes"
+;;;;;;  "flymake-python-pyflakes.el" (21207 45988 36729 118000))
+;;; Generated autoloads from flymake-python-pyflakes.el
+
+(autoload 'flymake-python-pyflakes-load "flymake-python-pyflakes" "\
+Configure flymake mode to check the current buffer's python syntax using pyflakes.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flymake-python-pyflakes-pkg.el") (21207
+;;;;;;  45988 155993 645000))
+
+;;;***
+
+(provide 'flymake-python-pyflakes-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-python-pyflakes-autoloads.el ends here
